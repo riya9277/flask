28751a02e6ac9771e3b7a6028c52defcb80ab341
riya <riyakv2020@gmail.com>
2024-11-14 11:11:26 +0530

    updating

--- app.py
+++ app.py
@@ -1,9 +1,85 @@
-from flask import Flask
+from flask import Flask,render_template,url_for,request, redirect
+from flask_sqlalchemy import SQLAlchemy
+#from datetime import datetime
+from datetime import datetime, timezone
+import os
 app=Flask(__name__)
-@app.route('/')
+
+# Get the absolute path to the current directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Set the database URI to use the absolute path
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(current_dir, 'test.db')
+
+db= SQLAlchemy(app)
+
+class Todo(db.Model):
+    id=db.Column(db.Integer , primary_key=True)
+    content = db.Column(db.String(200),nullable=False)
+    completed=db.Column(db.Integer,default=0)
+    date_created = db.Column(db.DateTime(timezone=True), default=datetime.now(timezone.utc))
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+# Create the tables manually
+if not os.path.exists(os.path.join(current_dir, 'test.db')):
+    with app.app_context():
+        db.create_all()
+
+@app.route('/',methods=['POST','GET'])
 def index():
-    return "hello world"
+    if request.method == 'POST':
+        task_content = request.form['content'].lower()
+
+        # Check if task content already exists
+        existing_task = Todo.query.filter_by(content=task_content).first()
+        if existing_task:
+            tasks = Todo.query.order_by(Todo.date_created).all()
+            return render_template('index.html', tasks=tasks ,msg = 'Task already exists')
+
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        try:
+            tasks = Todo.query.order_by(Todo.date_created).all()
+            return render_template('index.html', tasks=tasks)
+        except:
+            return 'No tasks found'
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
+
+    else:
+        return render_template('update.html', task=task)
+
 
 if __name__=="__main__":
-    app.run(debug="True")
-
+    app.run(debug=True)
